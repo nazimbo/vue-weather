ca979e77970eefc9662d5f12f434553b12d2059d
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-11-09 22:05:14 +0100

    Remove unused styles from App.vue to streamline code and improve maintainability

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,3 @@ import HomeView from './views/HomeView.vue'
     <HomeView />
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  background-color: #f0f2f5;
-}
-</style>
